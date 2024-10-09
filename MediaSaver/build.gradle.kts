015b2b7514490985e3add6378cf49ff48f39edcb
sunjin110 <sunjin110@gmail.com>
2024-10-09 09:01:20 +0900

    a

--- MediaSaver/build.gradle.kts
+++ MediaSaver/build.gradle.kts
@@ -13,9 +13,42 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
+tasks.register<Jar>("lambda") {
+    archiveFileName.set("lambda.jar")
+    manifest {
+        attributes["Main-Class"] = "cmd.lambda.MainKt"
+    }
+    from(sourceSets.main.get().output)
+
+    // 依存関係をjarに含める
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    })
+}
+
+tasks.register<Jar>("main") {
+    archiveFileName.set("main.jar")
+    manifest {
+        attributes["Main-Class"] = "cmd.main.MainKt"
+    }
+    from(sourceSets.main.get().output)
+
+    // 依存関係をjarに含める
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get()
+            .filter { it.name.endsWith("jar") }
+            .map { zipTree(it) }
+    })
+}
+
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
